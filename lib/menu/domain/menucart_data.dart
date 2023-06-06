3d887e697847371c132cd86092d8bddc88b9f3da
Phyu San <psan22582@gmail.com>
2023-06-06 16:34:03 +0630

    cart

---
+++ lib/menu/domain/menucart_data.dart
@@ -0,0 +1,58 @@
+class MenuCartData {
+  final int? id;
+  final String? productname;
+  final String? productphoto;
+  final int? price;
+
+  MenuCartData({this.id, this.productname, this.productphoto, this.price});
+}
+
+// Demo data for our cart
+
+List<MenuCartData> menu = [
+  MenuCartData(
+      id: 0,
+      productname: 'Roman Spicy Noodle',
+      productphoto: 'assets/images/europe food1.png',
+      price: 12000),
+  MenuCartData(
+      id: 1,
+      productname: 'Sweet PanCake',
+      productphoto: 'assets/images/morning food1.png',
+      price: 12000),
+  MenuCartData(
+      id: 2,
+      productname: 'Roman Spicy Noodle',
+      productphoto: 'assets/images/korea food1.png',
+      price: 12000),
+  MenuCartData(
+      id: 3,
+      productname: 'Roman Spicy Noodle',
+      productphoto: 'assets/images/japan food1.png',
+      price: 12000),
+  MenuCartData(
+      id: 4,
+      productname: 'Roman Spicy Noodle',
+      productphoto: 'assets/images/morning food3.png',
+      price: 12000),
+  MenuCartData(
+      id: 5,
+      productname: 'Roman Spicy Noodle',
+      productphoto: 'assets/images/europe food1.png',
+      price: 12000),
+  MenuCartData(
+      id: 0,
+      productname: 'Roman Spicy Noodle',
+      productphoto: 'assets/images/korea food1.png',
+      price: 12000),
+  MenuCartData(
+      id: 6,
+      productname: 'Roman Spicy Noodle',
+      productphoto: 'assets/images/japan food1.png',
+      price: 12000),
+  MenuCartData(
+      id: 7,
+      productname: 'Roman Spicy Noodle',
+      productphoto: 'assets/images/europe food1.png',
+      price: 12000)
+];
--- lib/menu/menuscreen.dart
+++ lib/menu/menuscreen.dart
@@ -1,4 +1,5 @@
 import 'package:delicious_menu/homepage/homepage.dart';
+import 'package:delicious_menu/menu/widgets/menu_cart.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,11 +11,20 @@ class MenuScreen extends StatefulWidget {
 }
 
 class _MenuScreenState extends State<MenuScreen> {
-  int index = 0;
+  int qty = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: const Color.fromARGB(255, 184, 219, 180),
+        bottomNavigationBar: Padding(
+            padding: const EdgeInsets.only(bottom: 10, left: 20, right: 20),
+            child: Container(
+                height: 50,
+                color: Colors.green.shade400,
+                child: const Center(
+                    child: Text('Order Cart',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w500))))),
         appBar: AppBar(
           backgroundColor: Colors.green.shade400,
           elevation: 0,
@@ -28,149 +38,11 @@ class _MenuScreenState extends State<MenuScreen> {
               child: const Icon(Icons.arrow_back)),
           title: const Text('Menu Book'),
         ),
-        body: Padding(
-            padding:
-                const EdgeInsets.only(left: 10, right: 10, top: 20, bottom: 20),
-            child: Container(
-                decoration: BoxDecoration(
-                    color: const Color(0xffFFF8D6),
-                    borderRadius: BorderRadius.circular(20)),
-                width: double.infinity,
-                height: 100,
-                child: Stack(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Container(
-                          width: 80,
-                          height: 80,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                          ),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(15),
-                            child: const Image(
-                                image:
-                                    AssetImage('assets/images/korea food1.png'),
-                                fit: BoxFit.cover),
-                          )),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 100,
-                        top: 10,
-                        bottom: 10,
-                        right: 10,
-                      ),
-                      child: SizedBox(
-                          width: 150,
-                          height: 100,
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 10),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text('Korea Spicy Noodle'),
-                                const SizedBox(height: 5),
-                                Row(
-                                  children: [
-                                    const Text('Extra food'),
-                                    const SizedBox(width: 10),
-                                    Container(
-                                        width: 30,
-                                        height: 30,
-                                        decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(30),
-                                        ),
-                                        child: const Icon(
-                                            Icons.add_circle_outline))
-                                  ],
-                                ),
-                                const SizedBox(height: 10),
-                                const Row(
-                                  children: [
-                                    Text('Price -'),
-                                    Text('7000 Ks'),
-                                  ],
-                                ),
-
-                                //end of column
-                              ],
-                            ),
-                          )),
-                    ),
-                    Padding(
-                        padding: const EdgeInsets.only(
-                            left: 260, top: 10, bottom: 10),
-                        child: Column(
-                          children: [
-                            Container(
-                                width: 40,
-                                height: 40,
-                                decoration: const BoxDecoration(
-                                    color: Colors.yellow,
-                                    borderRadius: BorderRadius.vertical(
-                                        top: Radius.circular(20))),
-                                child: IconButton(
-                                    onPressed: () {
-                                      index = index + 1;
-                                      print(index);
-                                      setState(() {});
-                                    },
-                                    icon:
-                                        const Icon(Icons.add_circle_outline))),
-                            Container(
-                                width: 40,
-                                height: 40,
-                                decoration: const BoxDecoration(
-                                    color: Color(0xffFF6D60),
-                                    borderRadius: BorderRadius.vertical(
-                                        bottom: Radius.circular(20))),
-                                child: IconButton(
-                                    onPressed: () {
-                                      if (index > 0) {
-                                        index = index - 1;
-                                      }
-                                      print(index);
-                                      setState(() {});
-                                    },
-                                    icon: const Icon(
-                                        CupertinoIcons.minus_circled))),
-                          ],
-                        )),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: Padding(
-                          padding: const EdgeInsets.only(right: 10),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Padding(
-                                  padding: EdgeInsets.only(top: 10, right: 15),
-                                  child: Text('Qty',
-                                      style: TextStyle(
-                                        fontSize: 20,
-                                      ))),
-                              const SizedBox(height: 10),
-                              Container(
-                                  width: 40,
-                                  height: 40,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20),
-                                    color: const Color(0xffA2A378),
-                                  ),
-                                  child: Center(
-                                      child: Text('$index',
-                                          style: const TextStyle(
-                                              color: Colors.white))))
-                            ],
-                          )),
-                    )
-                    //end of Stack
-                  ],
-                ))));
+        body: ListView.builder(
+          itemCount: 20,
+          itemBuilder: (context, index) {
+            return const MenuCartWidget();
+          },
+        ));
   }
 }
