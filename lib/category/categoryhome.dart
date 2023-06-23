4fe7b5d1f1779ddd1d93fe0335426ef40d07f1ea
Phyu San <psan22582@gmail.com>
2023-06-23 22:14:23 +0630

    category

--- lib/category/categoryhome.dart
+++ lib/category/categoryhome.dart
@@ -13,25 +13,95 @@ class CategoryPage extends StatelessWidget {
             child: Text('Welcome to Food House'),
           ),
         ),
-        body: Column(
+        body: GridView.count(
+          crossAxisCount: 3,
+          children: const [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: MenuCardWidget(),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: MenuCardWidget(),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: MenuCardWidget(),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: MenuCardWidget(),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: MenuCardWidget(),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: MenuCardWidget(),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: MenuCardWidget(),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: MenuCardWidget(),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: MenuCardWidget(),
+            )
+          ],
+        ));
+  }
+}
+
+class MenuCardWidget extends StatelessWidget {
+  const MenuCardWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 120,
+        height: 130,
+        color: Colors.white,
+        child: Stack(
           children: [
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.only(top: 20),
               child: Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.amber,
-                  child: Stack(
-                    children: [
-                      const Text('Food'),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10),
-                        child: Container(
-                            width: 80, height: 30, color: Colors.blue),
-                      )
-                    ],
-                  )),
-            )
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color(0xff98D8AA),
+                ),
+                width: double.infinity,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30),
+              child: SizedBox(
+                width: 60,
+                height: 60,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(60),
+                  child: const Image(
+                      image: AssetImage('assets/images/europe food1.png'),
+                      fit: BoxFit.cover),
+                ),
+              ),
+            ),
+            const Padding(
+                padding: EdgeInsets.only(top: 70, left: 10),
+                child: Text('Noodle Salad')),
+            const Padding(
+                padding: EdgeInsets.only(
+                  left: 20,
+                  top: 90,
+                ),
+                child: Text('6000 Ks'))
           ],
         ));
   }
